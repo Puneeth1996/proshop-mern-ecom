f8891e58b833a656d55f464ee026cae8499b5e0a
Puneeth1996 <puneeth1996p@gmail.com>
2025-04-04 21:46:10 +0530

    upated for cors issue

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
